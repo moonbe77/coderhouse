d8d8c5e9d4b9e838541f7b719bcaf0cbb44f3b8e
Berny <moonbe77@gmail.com>
2017-08-30 09:00:50 -0300

    validacion de datos

--- clase3/js/tpClase3.js
+++ clase3/js/tpClase3.js
@@ -10,23 +10,34 @@ var pedirDatos = function(){
      datos[0] = Number(prompt("Ingresa un numero"))
      datos[1] = Number(prompt("Ingresa otro numero"))
      datos[2] = prompt("Ingresa el tipo de operacion que desear realizar, +, -, *, /,")
-    calcular (datos[0],datos[1],datos[2])
+        
+     if (datos[0] != "" && datos[1] != "" && datos[2] != "" ){
+            calcular (datos[0],datos[1],datos[2])
+        }else{
+            pedirDatos()
+        }
 }
 
 var calcular=function(num1, num2, operacion){
-    if (operacion=="+"){
-      var res = num1 + num2  
-        }
-    if (operacion=="-"){
-        var res = num1 - num2  
-        }
-    if (operacion=="*"){
-        var res = num1 * num2  
-        }
-    if (operacion=="/"){
-        var res = num1 / num2  
-        }   
-    console.log(res)
+    switch (operacion) {
+        case "+":
+            var res = num1 + num2              
+            break;
+        case "-":
+            var res = num1 - num2            
+            break;
+        case "*":
+            var res = num1 * num2              
+            break;
+        case "/":
+            var res = num1 / num2              
+            break;
+    
+        default:
+        console.log("El Operador no es correcto")
+            break;
+    }    
+    console.log("El resultado de",num1,operacion,num2,"=", res)
     return res
 }
 
