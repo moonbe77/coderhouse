98e3e993428df53f918b34bbc869ffbde2e1cfb7
moonbe77 <moonbe77@gmail.com>
2017-09-04 15:04:18 -0300

    corecciones no agregue mucho mas

--- clase4/js/peleaClase.js
+++ clase4/js/peleaClase.js
@@ -14,15 +14,17 @@ var accionGolpear =  function (vidaOponente, golpeMax, nombreGolpea, nombreRecib
     var golpeAtacante = Math.round(Math.random() * golpeMax)
     var resto = vidaOponente - golpeAtacante
     console.log(nombreGolpea," golpeo", golpeAtacante , " la vida de ",nombreRecibe,"es ", vidaOponente, " le queda de vida ", resto )
-    return resto
+    return resto //devuelve el resto de vida del oponente
 }
 var juego= function(jugador1, jugador2){
     if(jugador1.turno){
+        //si es turno de golpear del jugador 1
         jugador2.vida = accionGolpear(jugador2.vida, jugador1.golpe, jugador1.nombre, jugador2.nombre)
         jugador2.turno = true
         jugador1.turno = false
         
     }else{
+        //si es turno del jugador 2
         jugador1.vida = accionGolpear(jugador1.vida, jugador2.golpe, jugador2.nombre, jugador1.nombre)
         jugador1.turno = true
         jugador2.turno = false
