c1bdeccf37f505078e61131621a771312cc53401
moonbe77 <moonbe77@gmail.com>
2017-09-03 21:49:10 -0300

    Update tpClase4.js
    editado desde codepen iani

--- clase4/js/tpClase4.js
+++ clase4/js/tpClase4.js
@@ -8,7 +8,7 @@ El objeto pelicula Debera tener ID, titulo, descripcion, año, duracion, actores
 Debera tener metodos para editar todas sus propiedades,
  menos su ID, y para los actores tendra que ir agregando uno a uno
 */
-var id=1
+
 function Pelicula (id,titulo,descripcion,anio,duracion,actores,director){
     this.id = id
     this.titulo = titulo
@@ -35,7 +35,7 @@ function Pelicula (id,titulo,descripcion,anio,duracion,actores,director){
     }
     this.modActor = function(){
         newActor = prompt("mod actor",this.actores)
-        this.actores = newActor
+        this.actores.push(newActor)
     }
     this.modDirector = function(){
         newDur = prompt("mod director",this.director)
@@ -45,13 +45,28 @@ function Pelicula (id,titulo,descripcion,anio,duracion,actores,director){
 }
 
 
-    titulo = prompt("ingresa un nombre")
+    /*titulo = prompt("ingresa un nombre")
     desc = prompt("ingresa una descripción")
     anio = prompt("ingresa el añ de la pelicula")
     dur = prompt("ingresa la duración en formato -10:15-")
     actores = prompt("ingresa nombre de los actores separado por ;")
-    dir = prompt("ingresa el nombre del director")   
+    dir = prompt("ingresa el nombre del director")*/
+var id=1
+var titulo = "Valerian and the City of a Thousand Planets"
+var desc = "A dark force threatens Alpha, a vast metropolis and home to species from a        thousand planets. Special operatives Valerian and Laureline must race to identify the marauding menace and safeguard not just Alpha, but the future of the universe."
+var anio = "2017"
+var dur = "02:17"
+var actores = ["Dane DeHaan","Cara Delevingne","Clive Owen"]
+var dir = "Luc Besson" 
 
-    var pelicula1= new Pelicula(id,titulo,desc,anio,dur,actores,dir)
+//creo el objeto pelicula1
+var pelicula1= new Pelicula(id,titulo,desc,anio,dur,actores,dir)
+
+//muestro los datos por pantall
+var mostrarPelicula = function(titulo){  
+  
+document.write('id: ',titulo.id,'<br />Titulo: ',titulo.titulo,'<br />Descripción: ',titulo.descripcion,'<br />Año: ',titulo.anio,'<br />Duración: ',titulo.duracion,'<br />Actores:',titulo.actores[0],'<br />Director: ',titulo.director)
+
+}
     
 
