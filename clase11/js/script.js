b5c36e84e7422e4189e951189b8220f8b279b2e7
moonbe77 <moonbe77@gmail.com>
2017-09-27 21:59:22 -0300

    jquery con el ejercicio del texto

--- clase11/js/script.js
+++ clase11/js/script.js
@@ -1,6 +1,28 @@
-$(document).ready(function () {
-    var parrafo = ""
-    var qtyLetras = parrafo.length
+/*
+**cargar texto desde textarea
+**al presionar una tecla escribiendo en el textarea recalcular los datos
+**al presionar el boton formatear texto dar espacio despues de los puntos y poner en mayuscula
+**
+*/
+
+app = function() {
+    var myApp = {};
+    myApp.init = function() {
+        $(document).ready(function() {
+            console.log("-----Se inicio  la app-----------");
+            analizar();
+            //cargarDatos();
+            //cargarDatosLocales();
+
+        });
+    }();
+    return myApp;
+}();
+
+
+var analizar = function () {
+    var parrafo = $("#parrafo").val();
+    var qtyLetras = parrafo.length  
     var qtyVocales = ""
     var qtyPuntos = ""
     
@@ -8,14 +30,26 @@ $(document).ready(function () {
         carcateres : "",
     }
 
-    $('#parrafo').keydown(function (e) {
-        parrafo = $("#parrafo").val();
+//contar letras
+    $('#qtyLetras').text(qtyLetras);
+
+    $('#parrafo').keyup(function (e) {
+        parrafo = $("#parrafo").val()
         qtyLetras = parrafo.length;        
-        $('#qtyLetras').text(qtyLetras); 
+         actualizarDatos(qtyLetras,"1","2","3")
         console.log(qtyLetras)           
-    });
+    })
     
-    //contar letras
-    
+//dar formato
+$("#formatear").on("click", function () {
+    alert(parrafo)
 });
-console.log($("#parrafo").val())        
+    
+}      
+
+var actualizarDatos = function (a,b,c,d){
+    $('#qtyLetras').text(a)
+    $('#qtyPuntos').text(b)
+    $('#qtyEspacios').text(c)
+    $('#qtyVocales').text(d)
+}
