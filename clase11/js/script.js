593a1c53caf4c0f75f2a8b2906585ae34468245c
Berny <moonbe77@gmail.com>
2017-09-29 10:42:59 -0300

    ejercicio jquery y texto, live correccion. falta mejorar aspecto html

--- clase11/js/script.js
+++ clase11/js/script.js
@@ -7,7 +7,7 @@
 var parrafo = $("#parrafo").val();
 
 var datosParrafo = {
-    qtyLetras :0 ,
+    qtyLetras : 0,
     qtyVocales : 0,
     qtyPuntos : 0,
     qtyEspacios : 0,
@@ -29,52 +29,74 @@ app = function() {
     return myApp;
 }();
 
-
-    $('#parrafo').keyup(function (e) {
-      analizar()                 
+var analizar = function () {
+    contarLetras()
+    contarDatos()    
+} 
+//accion al teclear texto en el textarea
+$('#parrafo').keyup(function (e) {
+      analizar()                
     })
 
-var analizar = function () {
-   parrafo = $("#parrafo").val()
+
+
+var contarLetras =function(){
+    parrafo = $("#parrafo").val()
 //cantida de letras
     letras = parrafo.length
     datosParrafo.qtyLetras = letras
-//cantidad de vocales
+    return letras
+}
+
+var contarDatos =function(){
+    //cantidad de vocales
     const regexVocales = /[aeiou]/i  
     var parrafoArray= parrafo.split("")
+    var vocales = 0
+    var espacios = 0
+    var puntos = 0
+    var newTexto = ""
 
     for (var i = 0; i < parrafoArray.length; i++) {
-    //Cantidad de vocales
-        if (regexVocales.test(parrafoArray[i])) {
-            datosParrafo.qtyVocales ++
-        }
-    //cantidad de espacios
-        if (parrafoArray[i] == " ") {
-            datosParrafo.qtyEspacios ++
-        }        
-    //formateado de letras    
-        if (parrafoArray[i] == "." ){
-            if(parrafoArray[i+1] != undefined){
-                var letra = parrafoArray[i+1]  //cree esta variable para ver si no me daba el error la funcion toUpperCase linea 36
-                 parrafoArray[i+1]=letra.toUpperCase() // me da error pero hace lo que tiene que hacer
-               parrafoArray[i] = ". "
-               datosParrafo.txtNew += parrafoArray[i]
+        //Cantidad de vocales
+            if (regexVocales.test(parrafoArray[i])) {
+                vocales ++                                
             }
-    //cantidad de puntos
-            datosParrafo.qtyPuntos ++ 
-        }else{
-            datosParrafo.txtNew += parrafoArray[i]
+        //cantidad de espacios
+            if (parrafoArray[i] == " ") {
+                espacios ++
+            }        
+            //formateado de letras    
+            if (parrafoArray[i] == "." ){
+                if(parrafoArray[i+1] != undefined){
+                    var letra = parrafoArray[i+1]  //cree esta variable para ver si no me daba el error la funcion toUpperCase linea 36
+                    parrafoArray[i+1]=letra.toUpperCase() // me da error pero hace lo que tiene que hacer
+                    parrafoArray[i] = ". "
+                    newTexto += parrafoArray[i]
+                }
+                //cantidad de puntos
+                puntos ++
+            }else{
+                newTexto += parrafoArray[i]
+            }
         }
-    }
-//
-    
-//dar formato
-$("#formatear").on("click", function () {
-    alert(parrafo)
-});
+        datosParrafo.qtyVocales = vocales
+        datosParrafo.qtyEspacios = espacios
+        datosParrafo.qtyPuntos = puntos
+        datosParrafo.txtNew = newTexto
 
-actualizarDatos(datosParrafo)
-}      
+        actualizarDatos(datosParrafo)
+}
+
+
+     
+
+//dar formato
+var darFormato = function() {
+    $("#formatear").on("click", function () {
+        alert(parrafo)
+})
+}
 
 var actualizarDatos = function (datosParrafo){
     $('#qtyLetras').text(datosParrafo.qtyLetras)
